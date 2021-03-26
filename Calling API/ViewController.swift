8f3ecbc521f4cb1e3fd24f530f2d1734bb1bc615
Kaus <abc@gmail.com>
2021-03-26 17:42:14 +0530

    Design Done Code to implement

--- Calling API/ViewController.swift
+++ Calling API/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tokenLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tokenLabel.text  = "hello"
     }
+    
+    
+    @IBAction func getTokenPressed(_ sender: Any) {
+        
+    // Create URL
+    let url = URL(string: "https://jsonplaceholder.typicode.com/todos/"+UITextField.text )
+    guard let requestUrl = url else { fatalError() }
 
+    // Create URL Request
+    var request = URLRequest(url: requestUrl)
+
+    // Specify HTTP Method to use
+    request.httpMethod = "GET"
+
+    // Send HTTP Request
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        
+        // Check if Error took place
+        if let error = error {
+            print("Error took place \(error)")
+            return
+        }
+        
+        // Read HTTP Response Status code
+        if let response = response as? HTTPURLResponse {
+            print("Response HTTP Status code: \(response.statusCode)")
+        }
+        
+        // Convert HTTP Response Data to a simple String
+        if let data = data, let dataString = String(data: data, encoding: .utf8) {
+            print("Response data string:\n \(dataString)")
+        }
+        
+    }
+    task.resume()
+        }
+   
+ 
+    @IBOutlet weak var TxtField: UITextField!
+    
+    
 
 }
 
